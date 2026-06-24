1cef71b9970d80b7adda4f12b4964f3c4956cafd
tienphan-oss <phannhattien1019@.com>
2026-06-24 11:30:05 +0700

    update something

--- include/EasyDifficulty.h
+++ include/EasyDifficulty.h
@@ -1,9 +1,13 @@
 #pragma once
 
-class Difficulty
+#include "Difficulty.h"
+
+class EasyDifficulty : public Difficulty
 {
 public:
-    virtual int getHiddenCount(int wordLength)=0;
-
-    virtual ~Difficulty(){}
+    int getHiddenCount(int wordLength) override
+    {
+        int count = wordLength * 20 / 100;
+        return (count > 0) ? count : 1;
+    }
 };
