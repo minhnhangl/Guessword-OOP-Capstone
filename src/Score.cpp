00742a36546ed7361e58cd5af5a68a8ec866d2fb
trananlich01967-dot <trananlich.01967@gmail.com>
2026-06-25 14:17:53 +0700

    Complete Score and Player module

---
+++ src/Score.cpp
@@ -0,0 +1,29 @@
+#include "../Score.h"
+
+Score::Score(int _points)
+{
+    points = _points;
+}
+
+void Score::addPoint(int pts)
+{
+    points += pts;
+}
+
+int Score::getPoints() const
+{
+    return points;
+}
+
+Score Score::operator+(const Score &other)
+{
+    Score temp;
+    temp.points = this->points + other.points;
+    return temp;
+}
+
+ostream& operator<<(ostream &out, const Score &s)
+{
+    out << s.points << " points";
+    return out;
+}
